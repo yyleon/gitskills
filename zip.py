2e5dbf8ed8cb5b090d6c9e40db4a9b8b551821bb
leon <leonyy2020@gmail.com>
2021-08-17 15:52:24 +0800

    add zip.py

---
+++ zip.py
@@ -0,0 +1,18 @@
+#encoding=gbk
+
+import zipfile
+import os
+
+def zip_files( files, zip_name ):
+    zip = zipfile.ZipFile( zip_name, 'w', zipfile.ZIP_DEFLATED )
+    for file in files:
+        print ('compressing', file)
+        zip.write( file )
+    zip.close()
+    print ('compressing finished')
+
+if __name__=='__main__':
+    # os.chdir('D:\µç¾º´ýÏ´×¢µ¥')
+    files = ['a.txt','b.txt','file1.txt']
+    zip_file = 'all.zip'
+    zip_files(files, zip_file)
