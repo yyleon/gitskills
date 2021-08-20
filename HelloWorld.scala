7831b6cf3de217bee7e1f713bf37ed46c6a0c2a8
leon <leonyy2020@gmail.com>
2021-08-20 12:56:10 +0800

    add helloworld.scala

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,6 +1,5 @@
-object HelloWorld {
-  
-   def main(args: Array[String]) {
-      println("Hello, world!") 
-   }
+object HelloWorld{
+    def main(args:Array[String]):Unit={
+        println("Hello world!")
+    }
 }
