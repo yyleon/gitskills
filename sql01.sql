01e8141689120887db7293d84d0389449c5ecd4d
leon <leonyy2020@gmail.com>
2021-09-08 22:48:58 +0800

    update git ignore rule

--- sql01.sql
+++ sql01.sql
@@ -1,22 +1,16 @@
-select 
-sum(valid_bet_amount) valid_bet_amount,
-sum(net_amount) net_amount
-from
-(select
-concat('yy-',m.name) member_name,
-concat('yy-',m1.name) top_name,
-a.valid_bet_amount,
-a.net_amount
-from
-(select member_id,
-sum(valid_bet_amount) valid_bet_amount,
-sum(net_amount) net_amount
-from ods_game_record
-where settle_time between '2021-09-01 00:00:00' and '2021-09-05 23:59:59'
-and flag=1
-group by member_id) a 
-left join ods_member m on m.id= a.member_id
-left join ods_member m1 on m.top_id= m1.id
-left join (select t.member_id,GROUP_CONCAT(tag.tag_name) tag_name from ods_member_tag t left join ods_tag tag on t.tags_id = tag.id group by t.member_id) t on a.member_id=t.member_id) t1
-where t1.top_name in ('bobviptg632')
-group by t1.top_name
+SELECT
+date(settle_time) bet_day,
+api_type,
+api_name,
+game_type,
+game_name,
+name,
+count(1) bet_number,
+sum(bet_amount) bet_amount,
+sum(net_amount) net_amount,
+sum(valid_bet_amount) valid_bet_amount
+FROM ods_game_record
+WHERE settle_time BETWEEN concat(date(date_sub(NOW(),interval 35 day)),' 00:00:00')
+AND concat(date(date_sub(NOW(),interval 1 day)),' 23:59:59')
+AND flag = 1 
+group by bet_day,api_type,api_name,game_type,game_name,name
